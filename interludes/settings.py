34ab025db2a3ca4c198671e304739968ca4ff1ba
Dorian Lesbre <dorian.lesbre@gmail.com>
2020-12-12 13:34:25 +0100

    Added a basic HTML view

--- interludes/settings.py
+++ interludes/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'interludes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'interludes', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
