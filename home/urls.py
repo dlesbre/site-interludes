1046b0550d46a5c9343cf10675d7064a78d2c373
Dorian Lesbre <dorian.lesbre@gmail.com>
2020-12-28 11:10:44 +0100

    Added a bunch of static pages

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.static_view, {"slug":"home"}, name = 'home'),
 	path('inscription/', views.static_view, {"slug":"inscription"}, name = 'inscription'),
+	path('activites/', views.static_view, {"slug":"activites"}, name = 'activites'),
+	path('faq/', views.static_view, {"slug":"faq"}, name = 'FAQ'),
 ]
