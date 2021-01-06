2e5170891d914efbee8af56854feb10497122612
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-01-06 10:07:31 +0100

    Added favicon.ico

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic import RedirectView
 from django.urls import path
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
 	path('inscription/', views.static_view, {"slug":"inscription"}, name = 'inscription'),
 	path('activites/', views.static_view, {"slug":"activites"}, name = 'activites'),
 	path('faq/', views.static_view, {"slug":"faq"}, name = 'FAQ'),
+	path('favicon.ico', RedirectView.as_view(url='/static/imgs/favicon.ico')),
 ]
