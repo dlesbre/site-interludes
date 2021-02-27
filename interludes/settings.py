96f5be0584978dac74cb7ed6cd7d631a63408546
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-02-27 16:55:05 +0100

    Fixed tabs instead of spaces

--- interludes/settings.py
+++ interludes/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'django.contrib.sitemaps',
-        'home.apps.HomeConfig',
-        'accounts',
+	'home.apps.HomeConfig',
+	'accounts',
 ]
 
 MIDDLEWARE = [
