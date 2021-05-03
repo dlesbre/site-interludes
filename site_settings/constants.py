ebced8dab9e6a95d409994a7f1336f39c136910f
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-05-03 16:42:09 +0200

    Added query string to force css reload on change

--- site_settings/constants.py
+++ site_settings/constants.py
@@ -5,3 +5,6 @@ WEBSITE_VERSION_DATE = "2021-04-30"
 WEBSITE_FULL_VERSION = "{} - {}".format(
 	WEBSITE_VERSION, WEBSITE_VERSION_DATE
 )
+
+# Update this to force reload of cached css
+CSS_VERSION = "1.0"
