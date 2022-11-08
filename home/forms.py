fecdb3f755abcb8da4b472229899d1e688f30f98
Théo Le Moigne <bleizi@crans.org>
2022-11-08 19:44:23 +0100

    add nb_murder and comment fields in inscription

--- home/forms.py
+++ home/forms.py
@@ -13,13 +13,15 @@ class InscriptionForm(FormRenderMixin, forms.ModelForm):
 			"school", "sleeps", # "mug",
 			"meal_friday_evening", "meal_saturday_morning", "meal_saturday_midday",
 			"meal_saturday_evening", "meal_sunday_morning", "meal_sunday_midday",
+			"nb_murder", "comment"
 		)
 
 	field_groups = [["school"], ["sleeps"], #["mug"],
 		[
 			"meal_friday_evening", "meal_saturday_morning", "meal_saturday_midday",
 			"meal_saturday_evening", "meal_sunday_morning", "meal_sunday_midday",
-		]
+		],
+		["nb_murder"],["comment"]
 	]
 
 	def save(self, *args, commit=True, **kwargs):
