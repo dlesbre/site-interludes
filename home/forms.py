11955089e101dcb8d52c38f9cdcaaee44307b500
Théo Le Moigne <bleizi@crans.org>
2022-11-09 20:08:42 +0100

    separate activities and meal/sleep in inscriptions

--- home/forms.py
+++ home/forms.py
@@ -21,7 +21,7 @@ class InscriptionForm(FormRenderMixin, forms.ModelForm):
 			"meal_friday_evening", "meal_saturday_morning", "meal_saturday_midday",
 			"meal_saturday_evening", "meal_sunday_morning", "meal_sunday_midday",
 		],
-		["nb_murder"],["comment"]
+		["nb_murder"], ["comment"]
 	]
 
 	def save(self, *args, commit=True, **kwargs):
