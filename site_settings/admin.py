429fee1d9b02abc73fd0ccf9564292fcc2a53928
Dorian Lesbre <dorian.lesbre@gmail.com>
2023-10-05 12:52:37 +0200

    Prettier settings page

--- site_settings/admin.py
+++ site_settings/admin.py
@@ -20,3 +20,53 @@ class SiteSettingsAdmin(SingletonModelAdmin):
 			return "<a href='%s'>download</a>" % (obj.file.url,)
 		else:
 			return "No attachment"
+
+	fieldsets = [
+		(
+			"Informations générales",
+			{
+				"fields": [
+					# "hosting_school",
+					"contact_email",
+					("date_start", "date_end"),
+					"affiche",
+				]
+			},
+		),
+		(
+			"Activités",
+			{
+				"fields": [
+					"activity_submission_open",
+					"show_host_emails",
+				]
+			},
+		),
+		(
+			"Planning",
+			{
+				"fields": [
+					"display_planning",
+					"planning_file",
+					"caption_red",
+					"caption_orange",
+					"caption_yellow",
+					"caption_green",
+					"caption_blue",
+					"caption_dark_blue",
+					"caption_black",
+				],
+				"description": 'Laissez une case "Légende" vide pour la masquer',
+			},
+		),
+		(
+			"Technique",
+			{
+				"fields": [
+					"allow_mass_mail",
+					"global_message",
+					"global_message_as_html",
+				]
+			},
+		),
+	]
