e29fcf26ce7f09e62ec2cadb76ca69653e4c123a
Dorian Lesbre <dorian.lesbre@gmail.com>
2024-10-20 16:52:04 +0200

    Update version number

--- site_settings/constants.py
+++ site_settings/constants.py
@@ -1,8 +1,8 @@
 # The website version number
-WEBSITE_VERSION = "2.2.0"
-WEBSITE_VERSION_DATE = "2022-09-10"
+WEBSITE_VERSION = "3.0.0"
+WEBSITE_VERSION_DATE = "2024-10-20"
 
 WEBSITE_FULL_VERSION = "{} - {}".format(WEBSITE_VERSION, WEBSITE_VERSION_DATE)
 
 # Update this to force reload of cached css
-CSS_VERSION = "1.0"
+CSS_VERSION = "1.1"
